83eae274bff5c7c8ad155f1ec7c6b8798c1db6d2
anjalydas <164157759+anjalydas@users.noreply.github.com>
2024-05-06 22:00:57 +0530

    Your message here

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const MainArticleImage = document.getElementById ('MainArticleImage')
+const firstArticleTitle = document.getElementById ('articleTitle')
+const firstArticleDescription = document.getElementById ('firstArticleDes')
+const latestArticles = document.getElementById ('latestArticles')
+
+fetch('https://newsapi.org/v2/everything?q=tesla&from=2024-04-05&sortBy=publishedAt&apiKey=386710e6bd73496aa7ef1221f7a68432')
+.then(response => response.json())
+ .then(data => {
+ const firstArticle = data.articles[0]
+firstArticleTitle.innerHTML = firstArticle.title
+firstArticleDescription.innerHTML = firstArticle.description
+MainArticleImage.src = firstArticle.urlToImage    
+
+  }
+ )
+   .catch(error => {
+ console.error('Error fetching news:', error);
+  news.innerHTML = "<p>Sorry, something went wrong while fetching the news.</p>";
+  });
